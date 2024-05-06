5f14b74fb78f64d53ca13bf0edde73e68c6f02e8
SHIVANSH KRISHNA <shivanshkrishna2004@gmail.com>
2024-05-07 05:28:38 +0530

    Update

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const io = require('socket.io')(10000, { cors: { origin: "*" } })
+const io = require("socket.io")(10000, { cors: { origin: "*" } });
 
-const users = {}
+const users = {};
 try {
-    io.on('connection', socket => {
-        socket.on('new-user-joined', name => {
-            users[socket.id] = name;
+  io.on("connection", (socket) => {
+    socket.on("new-user-joined", (name) => {
+      users[socket.id] = name;
 
-            socket.broadcast.emit('user-joined', name)
-        })
+      socket.broadcast.emit("user-joined", name);
+    });
 
-        socket.on('send', message => {
-            socket.broadcast.emit('recieve', { message: message, name: users[socket.id] })
-        })
+    socket.on("send", (message) => {
+      socket.broadcast.emit("recieve", {
+        message: message,
+        name: users[socket.id],
+      });
+    });
 
-        socket.on('disconnect', message => {
-            socket.broadcast.emit('left', users[socket.id]);
-            delete users[socket.id]
-        })
-
-    })
-    
+    socket.on("disconnect", (message) => {
+      socket.broadcast.emit("left", users[socket.id]);
+      delete users[socket.id];
+    });
+  });
 } catch (error) {
-    console.log(error)
-    
+  console.log(error);
 }
--- js/client.js
+++ js/client.js
@@ -1,55 +1,44 @@
+const socket = io("http://localhost:10000");
 
-const socket = io('http://localhost:10000');
+const form = document.getElementById("send-container");
 
-const form = document.getElementById('send-container')
-
-const messageInput = document.getElementById('messageinp')
+const messageInput = document.getElementById("messageinp");
 const messageContainer = document.querySelector(".container");
 
-var audio = new Audio('ting.mp3')
-
+var audio = new Audio("ting.mp3");
 
 const append = (message, position) => {
-    const messageElement = document.createElement('div');
-    messageElement.innerText = message;
-    messageElement.classList.add('message');
-    messageElement.classList.add(position);
-    messageContainer.append(messageElement);
-    messageContainer.scrollTop = messageContainer.scrollHeight;
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageElement.classList.add("message");
+  messageElement.classList.add(position);
+  messageContainer.append(messageElement);
+  messageContainer.scrollTop = messageContainer.scrollHeight;
 
-    if (position == 'left') {
-        audio.play();
-    }
-}
+  if (position == "left") {
+    audio.play();
+  }
+};
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  append(`You: ${message}`, "right");
+  socket.emit("send", message);
+  messageInput.value = " ";
+});
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const message = messageInput.value;
-    append(`You: ${message}`, 'right')
-    socket.emit('send', message);
-    messageInput.value = " "
-})
+const name = prompt("Enter Your Name To Join");
+socket.emit("new-user-joined", name);
 
+socket.on("user-joined", (data) => {
+  append(`${name} joined the chat`, "left");
+});
 
+socket.on("recieve", (data) => {
+  append(`${data.name}: ${data.message}`, "left");
+});
 
-const name = prompt("Enter Your Name To Join")
-socket.emit('new-user-joined', name)
-
-
-
-socket.on('user-joined', data => {
-    append(`${name} joined the chat`, 'left')
-})
-
-
-
-socket.on('recieve', data => {
-    append(`${data.name}: ${data.message}`, 'left')
-})
-
-
-socket.on('left', name => {
-    append(`${name} left the chat`, 'left')
-})
-
+socket.on("left", (name) => {
+  append(`${name} left the chat`, "left");
+});
